7e2055ed3bf5df22a1dd771207af9215bdfa432e
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-01-21 16:05:25 -0500

    fixed bug

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,64 @@
+arr1 = [1,2,3,4,5]
+arr2 = [6,7,8,9,10]
+arr3 = [4,3,6,7,2,1]
+
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
+    merged_arr = arrA + arrB
     return merged_arr
-
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    # if length of list is greater than 1 
+    if len(arr) > 1:
+        # find the middle, split the list into 2 halves
+        middle = len(arr) // 2
+        left = arr[:middle]
+        right = arr[middle:]
+
+        # use recursion for the 2 halves
+        merge_sort(left)
+        merge_sort(right)
+
+        # set variables to 0
+        a = 0
+        b = 0 
+        c = 0
+
+        # while 0 is less than both halves
+        while a < len(left) and b < len(right):
+            # if the left index is less than the right index
+            if left[a] < right[b]:
+                # assign left index
+                arr[c] = left[a]
+                # increment left
+                a += 1
+            else:
+                # else assign right index
+                arr[c] = right[b]
+                # increment right
+                b += 1
+            # increment through list 
+            c +=1
+
+        # while a is less than length of left half increment
+        while a < len(left):
+            arr[c] = left[a]
+            a += 1
+            c += 1
+
+        # while b is less than length of right half increment
+        while b < len(right):
+            arr[c] = right[b]
+            b += 1
+            c += 1
 
     return arr
+print(merge_sort(arr3))
 
 
 # STRETCH: implement an in-place merge sort algorithm
